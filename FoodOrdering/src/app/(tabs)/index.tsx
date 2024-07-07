73f1f8c32b61e1ed39065ca5eb3e6325079d8da3
anuragpatle <anurag.ap6@gmail.com>
2024-07-07 18:33:59 +0530

    First custom component

--- FoodOrdering/src/app/(tabs)/index.tsx
+++ FoodOrdering/src/app/(tabs)/index.tsx
@@ -1,44 +1,12 @@
-import { Text, StyleSheet, Image } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
-import {  View,  } from '../../components/Themed';
-import Colors from '@/src/constants/Colors';
+import {  View,  } from '@/src/components/Themed';
 import products from '@/assets/data/products';
-
-const product = products[1];
+import ProductListItem from '@/src/components/ProductListItem';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{uri: product.image}} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}> ${product.price} </Text>
+    <View>
+      <ProductListItem product={products[2]}/>
+      <ProductListItem product={products[1]}/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 10
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 1
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '600',
-    marginVertical: 10
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: 'bold'
-  }
-
-});
